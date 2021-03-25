af16cff10cb50af0ee57977c9c0238681d1e2201
Patrick Bender <johnpatrickbender@yahoo.com>
2021-03-24 19:58:07 -0700

    Day 2-Elliptic Curve Cryptography

--- 2-ECC/src/ECC/core.clj
+++ 2-ECC/src/ECC/core.clj
@@ -8,7 +8,6 @@
 )
 ;calculate inverse of x mod p
 (defn inverseMod [x m & {:keys [check] :or {check 0}}]
-  (println x m check)
   (if (< check m)
     (if (= 1 (mod (* check x) m))
       check
@@ -18,26 +17,43 @@
     
   )
 )
-(defn crossMod [P Q m]
-  (let [m (* 
-           (- (Q 1) (P 1))
-           (inverseMod (- (Q 0) (P 0)) m))
-        b (P 1)
-        
-        ]
-      
-
+(defn y [slope x b]
+  (+ (* slope x) b)
+  )
+(defn slopeDerivative [x y A m]
+  ;y' = 3x^2+A/(2y)
+  (*
+    (+ A (* 3 (* x x)))
+    (inverseMod (* 2 y) m)
   )
 )
-(defn multiplyMod [a P m]
-  (if (= a 0) 
-    P
-    (multiplyMod (- a 1) (crossMod(P P m)) p)
+(defn getSlope [P Q m A]
+  (if (= P Q)
+    (mod (slopeDerivative (P 0) (P 1) A m) m)
+    (mod (* 
+      (- (Q 1) (P 1))
+      (inverseMod (- (Q 0) (P 0)) m))
+      m
+    )
   )
 )
-(defn encrypt [msg curve p]
+(defn add [P Q A B m]
+    (let [slope (getSlope P Q m A)    
+          b (- (P 1) (* slope (P 0)))
+          y (fn [x] (+ (* slope x) b))
+          ;x_3 = slope^2 - x_1 - x_2
+          x_3 (mod (+ (* slope slope) (* -1 (P 0)) (* -1 (Q 0))) m)
+          ]
+        ;(println P Q slope b [x_3 (mod (* -1 (y x_3)) m)])
+        [x_3 (mod (* -1 (y x_3)) m)] 
+  )
 )
-(defn decrypt [msg curve p]
+;P original and p new, including two copies is ugly but it's easier than doing some fancy optional variable
+(defn multiply [a P_o A B m & {:keys [P_n] :or {P_n P_o}}]
+  (if (<= a 1) 
+    P_n
+    (multiply (- a 1) P_o A B m :P_n (add P_o P_n A B m))
+  )
 )
 (defn -main [& args]
   (def m 13)
@@ -47,20 +63,19 @@
   ;all possible P's are:
   ;(1,5) (1,8) (2,3) (2,10) (9,6) (9,7) (12,2) (12,11)
   (def P [2 10])
-  (def msg "Hello World")
-  ;n_a is Alice's secret key
-  (def n_A 15)
   ;Q_A is Alice's public key
-  (def Q_A 17)
-  ;k is Bob's ephemeral key
-  (def k (rand-int 30))
-  "I don't do a whole lot."
-  (println (discriminant -15 18))
-  ;Should be 5
-  (println (inverseMod 3 7))
-  ;Should be 0
-  (println (inverseMod 2 6))
-  ; should be [-23/9 -170/27]
-  ;(println (crossMod [7 16] [1 2] ))
+
+  (if (= 0 (discriminant A B))
+    (println "This is not an elliptic curve")
+    (do
+      (println "Should be [0 0]: " (multiply 9 [2 3] A B m))
+      (println "Should be [12 11]: " (multiply 2 [2 3] A B m))
+      (println "Should be [9 7]: "(multiply 3 [2 3] A B m))
+      (println "Should be [1 5]: " (multiply 4 [2 3] A B m))
+      (println "Should be [2 10]: " (multiply 8 [2 3] A B m))
+      (println "Should be [2 3]: " (add [1 5] [9 6] A B m))
+      (println "Should be [1 8]: " (add [12 2] [12 2] A B m))
+    )
+  )
 )
   
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 #Code from all my one-a-day projects
 
 1: 2d Physics simulator in clojure
+
 2: Elliptic curve cryptography in clojure
